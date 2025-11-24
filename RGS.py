614ed08876c81c45513ca86ebbf0c340e575feb4
Vibe <tryhardviber@gmail.com>
2025-11-24 20:47:30 +0000

    added basic command

--- RGS.py
+++ RGS.py
@@ -1,13 +1,17 @@
 import discord, os
 from dotenv import load_dotenv
-
+from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
+bot = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connnected to Discord!')
 
+@bot.command()
+async def ocr():
+    print("You tried to run an OCR!")
 client.run(TOKEN)
